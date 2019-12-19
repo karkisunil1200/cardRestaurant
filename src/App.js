7209bcbd25e7cd5c9408357db7c46ed1946cd5ad
Sunil Karki <karkisunil1200@gmail.com>
2019-12-19 12:02:18 -0600

    Signup component works

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './App.css';
 import LoginForm from './components/Login/LoginForm';
+import {Signup} from './components/Signup/Signup';
 
 function App() {
   return (
     <div className='App'>
-      <LoginForm />
+      <Signup />
     </div>
   );
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,17 +1,53 @@
-import react, {useState} from 'react';
+import React, {useState} from 'react';
 
 export const Signup = () => {
+  const [state, setState] = useState({
+    name: '',
+    city: '',
+    email: ''
+  });
 
-    const [value, setValue] = useState({
-        name: '',
-        city: '',
-        email: '',
-    })
-  <div>
-    <h1>Welcome to signup form</h1>
-    <form>
-      <lable>Enter first name</lable>
-      <input type='text' name='name' />
-    </form>
-  </div>;
+  const handleChange = e => {
+    setState({...state, [e.target.name]: e.target.value});
+    console.log([e.target.name], e.target.value);
+  };
+
+  return (
+    <div className='container'>
+      <h1>Welcome to signup form</h1>
+      <form>
+        <div className='inputs'>
+          <label>Enter name</label>
+          <input
+            type='text'
+            name='name'
+            onChange={handleChange}
+            value={state.name}
+            placeholder='Sunil Karki'
+          />
+        </div>
+        <div className='inputs'>
+          <label>Enter City</label>
+          <input
+            type='text'
+            name='city'
+            onChange={handleChange}
+            value={state.city}
+            placeholder='Kathmandu'
+          />
+        </div>
+
+        <div className='inputs'>
+          <label>Enter email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={handleChange}
+            value={state.email}
+            placeholder='sunil@gmail.com'
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
